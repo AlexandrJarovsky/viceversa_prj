b022ac429d90e9d5831077d90fe5756d6713fabb
AlJ <alex05.jar76@gmail.com>
2023-09-28 15:48:46 +0300

    Tri commit

--- viceversa/views.py
+++ viceversa/views.py
@@ -7,5 +7,8 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     revers_text = user_text[::-1]
+    count_words = len(user_text.split())
+    titl_text = f'Original Text for {count_words} words'
 #    print(test1)
-    return render(request, 'reverse.html', {'usertext': user_text, 'reverstext': revers_text})
+    return render(request, 'reverse.html', {'usertext': user_text, 'reverstext': revers_text,
+                                            'origtext': titl_text})
